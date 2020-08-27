6ef6f6ca9ff4f0c223cd1a0945a8fb7c0ebcbf45
Richard deMeester <richard@willdooit.com>
2020-08-27 11:34:34 +1000

    Further change to ruls

--- mcfix_sale/models/res_partner.py
+++ mcfix_sale/models/res_partner.py
@@ -2,12 +2,30 @@
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 # Copyright 2017 Creu Blanca
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import _, api, models
+from odoo import _, api, models, fields
 from odoo.exceptions import ValidationError
 
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
+
+    relevant_company_id = fields.Many2one('res.company', compute='_compute_relevant_company', search='_search_relevant_company')
+
+    @api.multi
+    def _compute_relevant_company(self):
+        for partner in self:
+            partner.relevant_company_id = partner.company_id
+
+    @api.model
+    def _search_relevant_company(self, operator, value):
+        assert operator == 'child_of'
+        assert isinstance(value, (list, tuple))
+        res = self.search([('company_id', operator, value)])
+
+        users = self.env['res.users'].search([('company_ids', operator, value)])
+        res |= users.mapped('partner_id')
+
+        return [('id', 'in', res.ids)]
 
     @api.multi
     @api.constrains('company_id')
